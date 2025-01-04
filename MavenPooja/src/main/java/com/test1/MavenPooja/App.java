4733f1e519aa13ad4aaf29ca1315261295ffe24a
pooja123g <pooja123g@gmail.com>
2025-01-04 23:37:15 +0100

    Adding new folder

---
+++ MavenPooja/src/main/java/com/test1/MavenPooja/App.java
@@ -0,0 +1,10 @@
+package com.test1.MavenPooja;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ MavenPooja/src/test/java/OpenPage.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OpenPage {
+
+	WebDriver driver;
+	
+	@Test
+	public void OpenBrowser() throws InterruptedException{
+		driver = new ChromeDriver(); 
+		driver.get("https://www.amazon.in");
+		
+		driver.manage().window().maximize();
+	
+		//WebElement selectdrpdwn = 
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone 16pro max");
+		driver.findElement(By.id("nav-search-submit-button")).click();
+		Thread.sleep(5000);		
+		driver.quit();
+	
+	}
+}
